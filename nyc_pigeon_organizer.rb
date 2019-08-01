bd0d5f9ac988245e09421059bd4bf956a3180f5b
German Garcia <ggarciag.business@outlook.com>
2019-08-01 10:03:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require('pry')
+def nyc_pigeon_organizer(pigeon_data)
+  new = Hash.new
+  pigeon_data.each do |key, sub_hash|
+    sub_hash.each do |sub_key, names|
+      names.each do |name|
+        if !!new[name] == false
+            # if the key is not yet in the Hash
+            new[name] = {}
+        end
+      end
+    end
+  end 
+  new
+end 
+
+
+
+def alternate
+  
 end
+
+
+
+ 
+
+
